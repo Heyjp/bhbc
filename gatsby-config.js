7e3fcf6da8510c5e1e4266fe344538375c0cc9cb
Jamie <jamieplayer@gmail.com>
2019-04-19 18:00:55 +0100

    updated strapi url

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-strapi',
       options: {
-        apiURL: 'http://localhost:1337',
+        apiURL: 'https://bhbc.herokuapp.com/',
         contentTypes: [ // List of the Content Types you want to be able to request from Gatsby.
           'article', 'file', 'fixture'
         ],
