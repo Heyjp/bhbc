40a6b4b8e4abc766fa9eb5cf0dc7bcf8e3d4133d
Jamie Player <jamieplayer@gmail.com>
2020-01-05 20:22:28 +0000

    url to publicURL on query

--- src/pages/app/minutes.js
+++ src/pages/app/minutes.js
@@ -1,62 +1,69 @@
 import React from "react"
-import { StaticQuery, graphql } from 'gatsby';
-import { brotliDecompress } from "zlib";
+import { StaticQuery, graphql } from "gatsby"
+import { brotliDecompress } from "zlib"
 
-const Minutes = (props) =>  {
-      return (
-        <div className="container">
-          <section className="content" >
-            <div className="columns minutes-wrap is-centered ">
-              <div className="column is-three-quarters minute-col">
-              <h3 className="subtitle is-3">Minutes</h3>
-              <hr/>
-              <table className="table is-fullwidth is-bordered is-striped">
-                  <thead>
-                      <tr>
-                          <td>Name</td>
-                          <td>Date</td>
-                          <td>Download Link</td>
-                      </tr>
-                  </thead>
-                  <tbody>
-                    {props.data.allStrapiMinute.edges.map(({node}) => {
-                        return (
-                      <tr key={node.id}>
-                        <td>{node.name}</td>
-                        <td>{new Date(node.date).toDateString()}</td>
-                        <td>
-                            <a className="button is-link" href={ node.file.url } download>Click here to download</a>
-                        </td>
-                      </tr>
-                    )
-                        })}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </section>
+const Minutes = props => {
+  return (
+    <div className="container">
+      <section className="content">
+        <div className="columns minutes-wrap is-centered ">
+          <div className="column is-three-quarters minute-col">
+            <h3 className="subtitle is-3">Minutes</h3>
+            <hr />
+            <table className="table is-fullwidth is-bordered is-striped">
+              <thead>
+                <tr>
+                  <td>Name</td>
+                  <td>Date</td>
+                  <td>Download Link</td>
+                </tr>
+              </thead>
+              <tbody>
+                {props.data.allStrapiMinute.edges.map(({ node }) => {
+                  return (
+                    <tr key={node.id}>
+                      <td>{node.name}</td>
+                      <td>{new Date(node.date).toDateString()}</td>
+                      <td>
+                        <a
+                          className="button is-link"
+                          href={node.file.publicURL}
+                          download
+                        >
+                          Click here to download
+                        </a>
+                      </td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+          </div>
         </div>
-      )
+      </section>
+    </div>
+  )
 }
 
-export default Minutes;
+export default Minutes
 
 export const query = () => (
   <StaticQuery
     query={graphql`
       query MinutesQuery {
         allStrapiMinute {
-            edges {
-                node {
-                    title
-                    date
-                    file {
-                        url
-                    }
-                }
+          edges {
+            node {
+              title
+              date
+              file {
+                publicURL
+              }
             }
+          }
         }
       }
-  `} render={Minutes} />
-);
-
+    `}
+    render={Minutes}
+  />
+)
