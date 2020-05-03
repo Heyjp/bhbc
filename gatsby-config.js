f3ab621777949313f6d9726a2f7a3968a14acb5d
Jamie <jamieplayer@gmail.com>
2020-05-03 22:22:03 +0100

    server url

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: `http://localhost:1337`,
+        apiURL: `http://bhbc.herokuapp.com`,
         singleTypes: [
           'committee'
         ],
